7df448076bae5df1f3ef828c3a116a3470f2ef08
kirti23456 <Srivastavakirti57@gmail.com>
2026-06-17 14:26:38 +0530

    initial build

---
+++ frontend/src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const App = () => {
+  return (
+      <div>AI Blogs
+    <ol>
+      <li>Agentic Ai</li>
+      <li>Generative Ai</li>
+      <li>LNM</li>
+    </ol></div>
+  )
+}
+
+export default App
